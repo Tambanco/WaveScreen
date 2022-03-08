70835460b8e175df2f0aff92a33f1ed321f66bc0
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-03-08 18:41:35 +0300

    Update WaveView.swift
    refactoring

--- WaveScreen/WaveModule/WaveView.swift
+++ WaveScreen/WaveModule/WaveView.swift
@@ -27,46 +27,58 @@ class WaveView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        layer.insertSublayer(layer1, at: 0)
-        layer.insertSublayer(layer2, at: 1)
-        layer.insertSublayer(layer3, at: 2)
+        drawLayer1()
+        drawLayer2()
+        drawLayer3()
         
-        layer1.opacity = 0.8
-        layer2.opacity = 0.7
-        layer3.opacity = 0.6
+        drawLayer4()
+        drawLayer5()
+        drawLayer6()
         
-        let path1 = UIBezierPath()
-        path1.move(to: CGPoint(x: 0, y: 0))
-        path1.addLine(to: CGPoint(x: frame.width, y: 0))
-        path1.addLine(to: CGPoint(x: frame.width, y: 175))
-        path1.addCurve(to: CGPoint(x: 0, y: 175), controlPoint1: CGPoint(x: 100, y: 350), controlPoint2: CGPoint(x: -100, y: -300))
-        path1.addLine(to: CGPoint(x: 0, y: 0))
+        func drawLayer1() {
+            let path1 = UIBezierPath()
+            path1.move(to: CGPoint(x: 0, y: 0))
+            path1.addLine(to: CGPoint(x: frame.width, y: 0))
+            path1.addLine(to: CGPoint(x: frame.width, y: 175))
+            path1.addCurve(to: CGPoint(x: 0, y: 175), controlPoint1: CGPoint(x: 100, y: 350), controlPoint2: CGPoint(x: -100, y: -300))
+            path1.addLine(to: CGPoint(x: 0, y: 0))
 
-        layer1.path = path1.cgPath
-        layer1.fillColor = UIColor.gray.cgColor
-        layer1.frame = bounds
+            layer1.path = path1.cgPath
+            layer1.fillColor = UIColor.gray.cgColor
+            layer1.opacity = 0.8
+            layer1.frame = bounds
+            layer.insertSublayer(layer1, at: 0)
+        }
         
-        let path2 = UIBezierPath()
-        path2.move(to: CGPoint(x: 0, y: 0))
-        path2.addLine(to: CGPoint(x: frame.width, y: 0))
-        path2.addLine(to: CGPoint(x: frame.width + 20, y: 220))
-        path2.addCurve(to: CGPoint(x: -100, y: 50), controlPoint1: CGPoint(x: 100, y: 400), controlPoint2: CGPoint(x: 150, y: 50))
-        path2.addLine(to: CGPoint(x: 0, y: 0))
+        func  drawLayer2() {
+            let path2 = UIBezierPath()
+            path2.move(to: CGPoint(x: 0, y: 0))
+            path2.addLine(to: CGPoint(x: frame.width, y: 0))
+            path2.addLine(to: CGPoint(x: frame.width + 20, y: 220))
+            path2.addCurve(to: CGPoint(x: -100, y: 50), controlPoint1: CGPoint(x: 100, y: 400), controlPoint2: CGPoint(x: 150, y: 50))
+            path2.addLine(to: CGPoint(x: 0, y: 0))
 
-        layer2.path = path2.cgPath
-        layer2.fillColor = UIColor.green.cgColor
-        layer2.frame = bounds
+            layer2.path = path2.cgPath
+            layer2.opacity = 0.7
+            layer2.fillColor = UIColor.green.cgColor
+            layer2.frame = bounds
+            layer.insertSublayer(layer2, at: 1)
+        }
         
-        let path3 = UIBezierPath()
-        path3.move(to: CGPoint(x: 0, y: 0))
-        path3.addLine(to: CGPoint(x: frame.width, y: 0))
-        path3.addLine(to: CGPoint(x: frame.width, y: 175))
-        path3.addCurve(to: CGPoint(x: 0, y: 175), controlPoint1: CGPoint(x: 100, y: 400), controlPoint2: CGPoint(x: -100, y: -400))
-        path3.addLine(to: CGPoint(x: 0, y: 0))
+        func drawLayer3() {
+            let path3 = UIBezierPath()
+            path3.move(to: CGPoint(x: 0, y: 0))
+            path3.addLine(to: CGPoint(x: frame.width, y: 0))
+            path3.addLine(to: CGPoint(x: frame.width, y: 175))
+            path3.addCurve(to: CGPoint(x: 0, y: 175), controlPoint1: CGPoint(x: 100, y: 400), controlPoint2: CGPoint(x: -100, y: -400))
+            path3.addLine(to: CGPoint(x: 0, y: 0))
 
-        layer3.path = path3.cgPath
-        layer3.fillColor = UIColor.blue.cgColor
-        layer3.frame = bounds
+            layer3.path = path3.cgPath
+            layer3.fillColor = UIColor.blue.cgColor
+            layer3.opacity = 0.6
+            layer3.frame = bounds
+            layer.insertSublayer(layer3, at: 2)
+        }
     }
     
     required init?(coder: NSCoder) {
